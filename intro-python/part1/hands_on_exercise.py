f1805e234a6c9e4bc35a5fd7fdf19c228397c3aa
M D <jcmatt123@gmail.com>
2021-12-25 19:37:08 -0700

    test

--- intro-python/part1/hands_on_exercise.py
+++ intro-python/part1/hands_on_exercise.py
@@ -7,19 +7,30 @@ import random
 
 # TODO: Write a print statement that displays both the type and value of `pi`
 pi = math.pi
+print("type of pi", type(pi), "value=", pi)
 
 
 # TODO: Write a conditional to print out if `i` is less than or greater than 50
 i = random.randint(0, 100)
+if i != 50:
+    print (i)
+
 
 
 # TODO: Write a conditional that prints the color of the picked fruit
 picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
+if picked_fruit != 'orange':
+    print ("picked fruit is ", picked_fruit)
+else:
+    print("orange")
 
 
 # TODO: Write a function that multiplies two numbers and returns the result
 # Define the function here.
+def multiply_number(a,b):
+    print (a , " x " , b , " = ", a*b)
 
+multiply_number(2,3)
 
 # TODO: Now call the function a few times to calculate the following answers
 
